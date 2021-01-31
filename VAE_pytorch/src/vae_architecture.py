720e0b44f213adb45dfb6d79ebd96de355650593
Mostafa Shehata <mostafa.shehata@seera.sa>
2021-01-31 13:44:11 +0400

    .

--- VAE_pytorch/src/vae_architecture.py
+++ VAE_pytorch/src/vae_architecture.py
@@ -30,11 +30,10 @@ class Encoder(nn.Module):
         self.log_var = nn.Linear(9216, k)
         
         self.leaky_relu = nn.LeakyReLU()
-        self.dropout = nn.Dropout2d(0.2)
+        self.dropout = nn.Dropout2d(0.25)
             
         
     def forward(self, x):
-        
         x = self.conv_0(x)
         x = self.batch_norm_0(x)
         x = self.leaky_relu(x)
@@ -63,13 +62,52 @@ class Encoder(nn.Module):
         return mu_vec, log_var_vec 
     
     
-#class Decoder(nn.Module):
-#    def __init__(self):
-#        super().__init__()
-#        self.conv_0 = torch.nn.ConvTranspose2d(in_channels = 32, out_channels = 3, kernel_size = 64
-#
-#    def forward(self, x):
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        self.conv_3 = nn.ConvTranspose2d(in_channels = 64, out_channels = 64, kernel_size = 8)
+        self.batch_norm_3 = nn.BatchNorm2d(num_features = 64)
+        
+        self.conv_2 = nn.ConvTranspose2d(in_channels = 64, out_channels = 64, kernel_size = 16)
+        self.batch_norm_2 = nn.BatchNorm2d(num_features = 64)
+        
+        self.conv_1 = nn.ConvTranspose2d(in_channels = 64, out_channels = 32, kernel_size = 32)
+        self.batch_norm_1 = nn.BatchNorm2d(num_features = 32)
+        
+        self.conv_0 = nn.ConvTranspose2d(in_channels = 32, out_channels = 3, kernel_size = 64)
+        self.batch_norm_0 = nn.BatchNorm2d(num_features = 3)
+        
+        self.leaky_relu = nn.LeakyReLU()
+        self.dropout = nn.Dropout2d(0.25)
+        self.sigmoid = nn.Sigmoid()
+        
 
+    def forward(self, x):
+        
+        x = x.view(x.shape[0], 64, 12, 12)
+        
+        x = self.conv_3(x)
+        x = self.batch_norm_3(x)
+        x = self.leaky_relu(x)
+        x = self.dropout(x)
+        
+        x = self.conv_2(x)
+        x = self.batch_norm_2(x)
+        x = self.leaky_relu(x)
+        x = self.dropout(x)
+        
+        x = self.conv_1(x)
+        x = self.batch_norm_1(x)
+        x = self.leaky_relu(x)
+        x = self.dropout(x)
+        
+        x = self.conv_0(x)
+        x = self.batch_norm_0(x)
+        x = self.leaky_relu(x)
+        x = self.dropout(x)
+        
+        x = self.sigmoid(x)
         
         
         
@@ -95,10 +133,10 @@ if __name__ == '__main__':
     print('output mu shape', mu_vec.shape)
     print('output lgo_var shape', log_var_vec.shape)
     
-#    
-#    encoded_img = encoded_img.permute(0,2,3,1).detach().numpy()[0]
-#    plt.imshow(encoded_img)
-#    plt.show()
+    
+    dec = Decoder()
+    decoded_imgs = dec(mu_vec)
+    
     
     
     
